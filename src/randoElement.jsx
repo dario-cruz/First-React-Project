8895eb668136beb3dd67d2d78cdcd218e9ece2fb
Dario Cruz <rio2012@gmail.com>
2023-07-23 09:39:07 -0400

    Add array that gets mapped to list items.

--- src/randoElement.jsx
+++ src/randoElement.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 function RandoElement() {
+    const listItems = ['Apple', 'Bananna', 'Orange', 'Guava', 'Pear']
+
     return (
         <div>
             <h1>Test title</h1>
             <ol className="test-list">
-                <li>List item 1</li>
-                <li>List item 2</li>
-                <li>List item 3</li>
+                {listItems.map((item) => {
+                    return <li key={item}>{item}</li>
+                })}
             </ol>
             <svg >
                <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
