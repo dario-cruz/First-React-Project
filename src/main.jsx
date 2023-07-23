6614fecb4f880097be863f8ba43543f009269ca1
Dario Cruz <rio2012@gmail.com>
2023-07-23 07:23:27 -0400

    Add RandoElement to main.jsx file.

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import HelloWorld from './helloworld.jsx'
 import Paragraph from './intro'
+import RandoElement from './randoElement'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelloWorld />
     <Paragraph />
+    <RandoElement />
   </React.StrictMode>,
 )
